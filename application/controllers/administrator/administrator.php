8f5655ea15c06f817eee90dbd0f7a6c2e521ae67
unknown <1207549344@qq.com>
2017-02-09 17:27:38 +0800

    upload CIrbac

---
+++ application/controllers/administrator/administrator.php
@@ -0,0 +1,11 @@
+<?php
+class administrator extends CI_Controller{
+
+  function __construct(){
+     parent::__construct();
+  }
+  
+  function index(){
+    $this->load->view('administrator/index');
+  }
+}
